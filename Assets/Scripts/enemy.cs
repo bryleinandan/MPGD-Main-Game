50c2db0cd32a3e245e5ffb5751b04bdb3d0e9862
jess <120227200+jrufus09@users.noreply.github.com>
2024-10-21 11:58:10 +0100

    enemy faces player. does not move towards. also i broke the fov system again

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 // using UnityEditor;
+using UnityEngine.AI;
+
+// Movement will only work when you bake a nav mesh into the scene,
+// make sure ai navigation package is installed.
 
 // Alert system. A number increases the longer the target stays in the fov zone.
 // (and decreases when outside of): maxed/being alerted means enemy becomes hostile
@@ -11,87 +15,6 @@ public enum AlertStage
     Aware,
     Alerted
 }
-
-
-//public class enemy : MonoBehaviour
-//{
-//    public float fov;
-//    [Range(0, 360)] public float fovAngle; // angle of cone of vision, degrees
-
-//    public AlertStage alertStage;
-//    [Range(0, 100)] public float alertLevel; // 0=peaceful, 100=alerted
-
-//    private void Awake()
-//    {
-//        alertStage = AlertStage.Peaceful;
-//        alertLevel = 0;
-//    }
-
-//    //private void onDrawGizmos() // draw fov - wont work in 2023.1
-//    //{
-//    //    Handles.color = new Color(0, 1, 0, 0.3f);
-//    //    Handles.DrawSolidDisc(transform.position, transform.up, fov); // drawsolidarc
-//    //}
-
-//    private void Update()
-//    {
-//        bool playerInFov = false;
-//        // get array of things colliding with fov sphere
-//        Collider[] targetsInFOV = Physics.OverlapSphere(
-//            transform.position, fov);
-//        foreach (Collider c in targetsInFOV) // if there exists an object with player tag in the list of colliders
-//        {
-//            if (c.CompareTag("Player"))
-//                // calc angle
-//                float signedAngle = Vector3.Angle(
-//                    transform.forward,
-//                    c.transform.position - transform.position);
-//                if (Mathf.Abs(signedAngle) < fovAngle / 2)
-//                    playerInFOV = true;
-//                break;
-//        }
-//        _UpdateAlertState(playerInFov);
-//    }
-
-//    private void _UpdateAlertState(bool playerInFOV)
-//    {
-//        switch(alertStage)
-//        {
-//            case AlertStage.Peaceful:
-//                if (playerInFOV)
-//                    alertStage = AlertStage.Aware;
-//                break;
-
-//            case AlertStage.Aware: // increment if in fov, decrement if not
-//                if (playerInFOV)
-//                    alertLevel++;
-//                    if (alertLevel >= 100)
-//                        alertStage = AlertStage.Alerted;
-//                else
-//                    alertLevel--;
-//                    if (alertLevel <= 0)
-//                        alertStage = AlertStage.Peaceful;
-//                break;
-
-//            case AlertStage.Alerted: // decrement if not in fov
-//                if (!playerInFOV)
-//                    alertStage = AlertStage.Aware;
-//                break;
-//        }
-//    }
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-        
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-        
-//    }
-//}
 
 public class FieldOfView : MonoBehaviour
 { 
@@ -110,6 +33,9 @@ public class FieldOfView : MonoBehaviour
 
     public GameObject playerRef;
 
+    private NavMeshAgent agent;
+    public float enemyDistance = 0.7f;
+
     public LayerMask targetMask;
     public LayerMask obstructionMask;
     public bool canSeePlayer;
@@ -118,6 +44,7 @@ public class FieldOfView : MonoBehaviour
     {
         playerRef = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(FOVRoutine());
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private IEnumerator FOVRoutine() // core routine to reduce number of calls per frame (for performance)
@@ -135,35 +62,57 @@ public class FieldOfView : MonoBehaviour
 
     private void FieldOfViewCheck()
     {
+        Transform target = null;
         Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
         // array of things that collide with your fov cone
+        // henceforth we can refer to the rangeChecks array for a list 
         
         // do not ask why Length must be capitalised here. blame 2023.1
         if (rangeChecks.Length != 0)
         {
-            Transform target = rangeChecks[0].transform; // this only gets the first in rangecheck
-            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            // Transform target = rangeChecks[0].transform; // this only gets the first in rangecheck
+            // Identify player in the array
 
-            // check for angle
-            if ((Vector3.Angle(transform.forward, directionToTarget)) < angle / 2)
+            foreach (Collider c in rangeChecks) // if there exists an object with player tag in the list of colliders
             {
-                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+                if (c.CompareTag("Player"))
+                    target = c.transform;
+                //break; // exit
+            }
 
-                // limit raycast distance to whenever it hits something (vision is blocked by walls)
-                if (!(Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)))
-                    canSeePlayer = true;
-                else
-                    canSeePlayer = false;
+            if (target == null) // target is null, so set nav agent velocity to zero as player isn't in range
+            {
+                gameObject.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
             }
             else
-                canSeePlayer = false; // player is not within radius
-        }
-        else // check failed
+            {
+                //Vector3 directionToTarget = (target.postion - transform.position).normalized;
+                Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+                // check for angle
+                if ((Vector3.Angle(transform.forward, directionToTarget)) < angle / 2)
+                {
+                    float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                    // limit raycast distance to whenever it hits something (vision is blocked by walls)
+                    if (!(Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)))
+                    {
+                        canSeePlayer = true;;
+                    }
+                    else
+                        canSeePlayer = false;
+                }
+                else
+                    canSeePlayer = false; // player is not within radius
+            }
+            //else // check failed - for example there are no colliders in the list
             canSeePlayer = false;
-        // if check failed and you were previously viewing, you are no longer able to view player
+            // if check failed and you were previously viewing, you are no longer able to view player
 
+            _UpdateAlertState(canSeePlayer);
+            moveTowardsPlayer(target);
+        }
 
-        _UpdateAlertState(canSeePlayer);
     }
 
     private void _UpdateAlertState(bool playerInFOV)
@@ -198,5 +147,20 @@ public class FieldOfView : MonoBehaviour
         }
     }
 
+    private void moveTowardsPlayer(Transform player)
+    {
+        // move towards player
 
+        transform.LookAt(player);
+
+        agent.SetDestination(player.transform.position);
+
+        // if distance between player and self is 0
+        if (Vector3.Distance(transform.position, player.position) < enemyDistance)
+        {
+            gameObject.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
+            // and attack
+        }
+    }
+    // private void idle()
 }
