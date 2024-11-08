38bc302141c8a191870fee17f53518b801493d8a
Sahra <ec22951@qmul.ac.uk>
2024-11-08 19:17:55 +0000

    changed health script to work on enemy objects

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -6,13 +6,13 @@ using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
-    public Player player;
+    public Health playerHealth;
     void Start() {
         gameObject.SetActive(true);
         gameObject.transform.localScale = Vector3.zero; // hide game over screen
     }
     void Update() {
-        if (player.currentHealth <= 0) { // when player health 0, show game over screen
+        if (playerHealth.currentHealth <= 0) { // when player health 0, show game over screen
             gameObject.transform.localScale = Vector3.one;
         }
     }
