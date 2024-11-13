4082df005a75bbc74a981aaa53e3b8187cc1bff8
jess <120227200+jrufus09@users.noreply.github.com>
2024-11-13 20:15:35 +0000

    interaction physics work
    player can now recognise when objects on the interactable layer are nearby (not functionality as of yet).

--- Assets/Scripts/Interaction/Interactor.cs
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+// This goes onto whatever will be the active participant in the interaction
+// basically. the player.
 
 public class Interactor : MonoBehaviour 
 {
 [SerializeField] private Transform _interactionPoint; 
-[SerializeField] private float _interactionPointRadius = 0.5f;
+[SerializeField] private float _interactionPointRadius = 1.2f;
 [SerializeField] private LayerMask _interactableMask;
 
-private readonly Collider[] _colliders = new Collider[3];
+//private readonly Collider[] _colliders = new Collider[3];
+[SerializeField] private Collider[] _colliders = new Collider[3];
 [SerializeField] private int _numFound;
     
     void Start()
@@ -19,7 +24,7 @@ private readonly Collider[] _colliders = new Collider[3];
 
     void Update()
     {
-        // generate Sphere of Checking
+        // generate Sphere of Checking - check collisions with everything on layer (interactable mask)
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
 
         // if array not empty, get the first item in colliders & call Interact function from it
@@ -28,6 +33,8 @@ private readonly Collider[] _colliders = new Collider[3];
             if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame) {
                 interactable.Interact(this); // pass self in as a parameter
             }
+        } else {
+            _colliders[0] = null;
         }
     }
 
