7399b731232b6a272c26f35ba285d3e4107b0ef0
jess <120227200+jrufus09@users.noreply.github.com>
2024-11-14 23:03:08 +0000

    moving interaction prompt functionality to interactable.cs

--- Assets/Scripts/Interaction/InteractionPromptUI.cs
+++ Assets/Scripts/Interaction/InteractionPromptUI.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class InteractionPromptUI : MonoBehaviour
 {
+    // UI look at camera - parent some text to the camera
+
+    [SerializeField] private Camera _mainCam;
+    [SerializeField] private TextMeshProUGUI _promptText;
+
+    public bool isVisible = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _mainCam = Camera.main;
+        //private TextMeshProUGUI _promptText;
+        //text.SetActive(false)
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate() {
+        var rotation = _mainCam.transform.rotation;
+        transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+
+    public void SetUp(string promptText) {
+        _promptText.text = promptText;
+        isVisible = true;
+    }
+
+    public void Close() {
+        isVisible = false;
     }
 }
--- Assets/Scripts/Interaction/Interactor.cs
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -11,13 +11,15 @@ public class Interactor : MonoBehaviour
 {
 
 public KeyCode interactKey;
-[SerializeField] private Transform _interactionPoint; 
-[SerializeField] private float _interactionPointRadius = 1.2f;
-[SerializeField] private LayerMask _interactableMask;
+[SerializeField] private Transform interactionPoint; 
+[SerializeField] private float interactionPointRadius = 1.2f;
+[SerializeField] private LayerMask interactableMask; // layer to check for collisions
+[SerializeField] private InteractionPromptUI interactionPromptUI;
 
 //private readonly Collider[] _colliders = new Collider[3];
-[SerializeField] private Collider[] _colliders = new Collider[3];
+[SerializeField] private Collider[] colliders = new Collider[3];
 [SerializeField] private int _numFound;
+private IInteractable interactable;
     
     void Start()
     {
@@ -27,26 +29,35 @@ public KeyCode interactKey;
     void Update()
     {
         // generate Sphere of Checking - check collisions with everything on layer (interactable mask)
-        _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
+        _numFound = Physics.OverlapSphereNonAlloc(interactionPoint.position, interactionPointRadius, colliders, interactableMask);
 
         // if array not empty, get the first item in colliders & call Interact function from it
         if (_numFound > 0) {
-            var interactable = _colliders[0].GetComponent<IInteractable>();
-            //if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame) {
-            if (Input.GetKeyDown(interactKey)) { // if false and true
-                Debug.Log("i have been interacted with.");
-                if (interactable != null) {
-                    interactable.Interact(this); // pass self in as a parameter
+            interactable = colliders[0].GetComponent<IInteractable>();
+            if (interactable != null) {
+
+                // found but not interacted with, show gui prompt
+                if (!interactionPromptUI.isVisible) {
+                    interactionPromptUI.SetUp(interactable.InteractionPrompt);
+                }
+
+                //if (interactable != null && Keyboard.current.eKey.wasPressedThisFrame) {
+                // found + keyboard pressed
+                if (Input.GetKeyDown(interactKey)) {
+                    Debug.Log("i have been interacted with.");
+                    interactable.Interact(this); // pass self in as a parameter to interact with the thing
                 }
             }
-        } else {
-            _colliders[0] = null;
+        } else { // number of things found is 0: nothing in vicinity detected
+            colliders[0] = null;
+            interactable = null;
+            interactionPromptUI.Close();
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_interactionPoint.position, _interactionPointRadius);
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionPointRadius);
     }
 }
