d9b8b82fb9d13651d092e1e9f0e6d210f35618a9
jess <120227200+jrufus09@users.noreply.github.com>
2024-11-23 19:19:14 +0000

    i think i will turn health bar into an interface

---
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class PlayerHealthBar : HealthBar
+// Inherits eveything in health bar base class.
+{
+
+    // public Slider slider;
+    // public Gradient gradient;
+    // public Image fill;
+
+    // public void SetMaxHealth(int health) {
+    //     slider.maxValue = health;
+    //     slider.value = health;
+
+    //     fill.color = gradient.Evaluate(1f);
+    // }
+    // public void SetHealth(int health) {
+    //     slider.value = health;
+    //     fill.color = gradient.Evaluate(slider.normalizedValue);
+    // }
+
+    // public void TakeDamage(int damage) {
+    //     slider.value = slider.value - damage;
+    //     //slider.value = Mathf.Lerp(minimum, maximum, t), 0, 0);
+    // }
+}
