fd04ce3c19749c7acb4be4086f1ab31981fb635b
jess <120227200+jrufus09@users.noreply.github.com>
2024-11-25 12:12:17 +0000

    i made a spawner however it does not work

--- Assets/Scripts/enemy/EnemyFOV.cs
+++ Assets/Scripts/enemy/EnemyFOV.cs
@@ -232,16 +232,40 @@ public class FieldOfView : MonoBehaviour, IAttack {
 } // end of class
 
 public class Enemy : MonoBehaviour {
-    public FieldOfView fieldOfView;
+    public FieldOfView fieldOfView; 
+    private Health healthComponent;
 
-    void Awake()
-    {
+    void Awake() {
         fieldOfView = new FieldOfView();
         fieldOfView.Initialize();
     }
 
-    void Update()
-    {
+    void Start() {
+        healthComponent = GetComponentInChildren<Health>();
+    }
+    
+    void Update() {
         // Use fieldOfView for enemy logic
+
+        if(healthComponent.currentHealth == 0) {
+            setToDestroy = true;
+        }
+    }
+
+    public float smoothSpeed = 2.5f;
+    public bool setToDestroy = false;
+    void LateUpdate() {
+        
+        // get small
+        if (setToDestroy) {
+            var smooth = 1.0f - Mathf.Pow(0.5f, Time.deltaTime * smoothSpeed);
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(0.0f, 0.0f, 0.0f), smooth*2);
+        }
+    }
+
+    public virtual void SelfDestruct() {
+        // this can overwritten in any child class by public override void selfDestruct() ...
+        Debug.Log("self destructing.");
+        Destroy(gameObject);
     }
 }
