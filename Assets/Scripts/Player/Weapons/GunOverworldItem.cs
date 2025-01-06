9268fab0e3d283bb18ad489b2bbe8dfebd65907a
jess <120227200+jrufus09@users.noreply.github.com>
2025-01-06 00:23:02 +0000

    why are so many items falling out

--- Assets/Scripts/Player/Weapons/GunOverworldItem.cs
+++ Assets/Scripts/Player/Weapons/GunOverworldItem.cs
@@ -6,9 +6,11 @@ public class GunOverworldItem : OverworldItem {
 
     [Header("Make sure first child is Canvas if second child is Model")]
     public Vector3 meshSize;
+    public MeshFilter parentMesh;
 
     protected override void Start() {
         selfMeshFilter = this.transform.GetChild(1).GetComponentInChildren<MeshFilter>();
+        parentMesh = selfMeshFilter;
         var model = selfMeshFilter.sharedMesh;
         meshSize = new Vector3((model.bounds.size.x * transform.localScale.x),
         (model.bounds.size.y * transform.localScale.y), (model.bounds.size.z * transform.localScale.z));
